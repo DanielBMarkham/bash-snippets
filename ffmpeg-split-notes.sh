8bff9b0ea37d82d2c64adb443c8a60945e057db0
Daniel Markham <danielbmarkham@hotmail.com>
2022-02-10 16:17:18 -0500

    notes on splitting

---
+++ ffmpeg-split-notes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+ffmpeg -ss 00:00:00 -t 00:50:00 -i largefile.mp4 -acodec copy \
+-vcodec copy smallfile.mp4
+
+652
+744
+751
+
+ffmpeg -ss 00:06:52 -t 00:00:59 -i 2022-02-11-nerd-roundup.mp4 2022-02-11-nerdster.mp4
+
+
+ffmpeg -ss 00:06:52 -t 00:00:59 -i 2022-02-11-nerd-roundup.mp4 2022-02-11-nerdster.mp4
+
